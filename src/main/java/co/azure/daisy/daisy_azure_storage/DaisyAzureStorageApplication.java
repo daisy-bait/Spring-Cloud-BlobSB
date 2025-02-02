f0544c31183e0e59c7cac3cec2a603f354f6bca7
daisy-dev <kadanarpa@gmail.com>
2025-02-01 21:36:34 -0500

    first commit: basic structure to work with azureSB

---
+++ src/main/java/co/azure/daisy/daisy_azure_storage/DaisyAzureStorageApplication.java
@@ -0,0 +1,13 @@
+package co.azure.daisy.daisy_azure_storage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaisyAzureStorageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DaisyAzureStorageApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/azure/daisy/daisy_azure_storage/DaisyAzureStorageApplicationTests.java
@@ -0,0 +1,13 @@
+package co.azure.daisy.daisy_azure_storage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DaisyAzureStorageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
